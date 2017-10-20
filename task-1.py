e7e8a4c774000fa9a76987ce0bc1f2723050b156
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 14:09:46 -0300

    printing the len of each word in the file

--- task-1.py
+++ task-1.py
@@ -3,12 +3,10 @@ y = open("Book2.txt", "r") #open second file
 z = open("Book2.txt", "r") #open third file
 
 
-def find_big(a):  			# this function will check the  biggest in all the books
-	m =0
+def find_big(a):  			# this function will check the  biggest in all the books):
+	longest_word = ''
 	for word in a.read().split():
-#		print(word)
-		for i in word:
-#			print (i)
-			q = max(word.split(), key=len)
-		print (q)
+		print(word, len(word))
+
+
 find_big(x)
