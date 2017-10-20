2f9466929ec97be7bb23d4c07d11eb9398ed05be
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 15:08:44 -0300

    finding the day for highest and lowest temp

--- task-2.py
+++ task-2.py
@@ -7,15 +7,18 @@ def api_pro():
 	condition = location.condition()
 	a = condition["temp"]
 	b = condition["temp"]
+	w = " "
+	q = " "
 	i =0
 	for forecasts in location.forecast():
 		if a < forecasts["high"]:
 			q = forecasts["date"]
 			a = forecasts['high']
-		if b < forecasts['low']:
-			b = forecasts['low']
-			
-	print ("The highest temperature in the next 5 days of forecast is : ", a)
-	print ("the lowest temperature in the next 5 days of forecast is : ", b)
+		if b < forecasts["low"]:
+			b = forecasts["low"]
+			w = forecasts["date"]
+	print ("The highest temperature in the next 5 days of forecast is  ", a, " and the day is " , q)
+	print ("the lowest temperature in the next 5 days of forecast is  ", b, " and the day is ", w)
+
 
 api_pro()
