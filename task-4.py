b2ac87c7a665689bfc54fce043d36768e3d31ef7
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 15:37:21 -0300

    creating the function for apic-em

--- task-4.py
+++ task-4.py
@@ -3,3 +3,10 @@ import requests
 import json
 
 controller='devnetapi.cisco.com/sandbox/apic_em'
+def getTicket():
+	url = "https://" + controller + "/api/v1/ticket"
+	payload = {"username":"devnetuser","password":"Cisco123!"}
+	header = {"content-type": "application/json"}
+	response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+	print (response)
+getTicket()
