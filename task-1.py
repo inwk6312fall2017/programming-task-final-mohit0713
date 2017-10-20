520f038602a536af94896b10422a6c62a7bbba9d
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 14:14:04 -0300

    finding the longest word in a file

--- task-1.py
+++ task-1.py
@@ -6,7 +6,9 @@ z = open("Book2.txt", "r") #open third file
 def find_big(a):  			# this function will check the  biggest in all the books):
 	longest_word = ''
 	for word in a.read().split():
-		print(word, len(word))
+		if len(word)> len(longest_word):
+			longest_word =  word
+	print(longest_word, len(longest_word))
 
 
 find_big(x)
