5ae646506b131852259303d95f9daaae4ca5c984
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 14:00:03 -0300

    using max function

--- task-1.py
+++ task-1.py
@@ -4,7 +4,11 @@ z = open("Book2.txt", "r") #open third file
 
 
 def find_big(a):  			# this function will check the  biggest in all the books
+	m =0
 	for word in a.read().split():
-		print(word)
-
+#		print(word)
+		for i in word:
+#			print (i)
+			q = max(word.split(), key=len)
+		print (q)
 find_big(x)
