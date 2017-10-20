c9f3490407c9d8a8070ab4274333bc4183a26249
mohit0713 <mohit.sharma@dal.ca>
2017-10-20 13:50:42 -0300

    using split to method

--- task-1.py
+++ task-1.py
@@ -1,3 +1,10 @@
 x = open("Book1.txt", "r") #open first file
 y = open("Book2.txt", "r") #open second file
 z = open("Book2.txt", "r") #open third file
+
+
+def find_big(a):  			# this function will check the  biggest in all the books
+	for word in a.read().split():
+		print(word)
+
+find_big(x)
